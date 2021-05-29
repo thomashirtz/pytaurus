d7be49b7842b2e42138e9b9e9eac7e455f7e7fc8
Thomas <hirtzt@gmail.com>
2021-05-29 17:03:37 +0200

    Add verbose and edit typing

--- pytaurus/project.py
+++ pytaurus/project.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
-from typing import List, Optional
+from typing import List
+from typing import Optional
 
 
 class Project:
@@ -8,31 +9,40 @@ class Project:
         self.path = path
         self.custom_environment = environment
 
-    def gsub(self, nodes: Optional[List[int]] = None, display_output: bool = False, environment: Optional[dict] = None):
-        cmd = self.get_gsub_cmd(nodes)
+    def gsub(self, nodes: Optional[List[int]] = None, display_output: bool = False, verbose: bool = False,
+             environment: Optional[dict] = None) -> int:
+        cmd = self.get_gsub_cmd(nodes, verbose)
         return self.execute_cmd(cmd, display_output, environment)
 
-    def run(self, nodes: Optional[List[int]] = None, display_output: bool = False, environment: Optional[dict] = None):
-        return self.gsub(nodes, display_output, environment)
+    def run(self, nodes: Optional[List[int]] = None, display_output: bool = False, verbose: bool = False,
+            environment: Optional[dict] = None) -> int:
+        return self.gsub(nodes, display_output, verbose, environment)
 
-    def gcleanup(self, nodes: Optional[List[int]] = None, renumbers_nodes: bool = False, display_output: bool = False, environment: Optional[dict] = None):
-        cmd = self.get_gcleanup_cmd(nodes, renumbers_nodes)
+    def gcleanup(self, nodes: Optional[List[int]] = None, renumbers_nodes: bool = False, display_output: bool = False,
+                 verbose: bool = False, environment: Optional[dict] = None) -> int:
+        cmd = self.get_gcleanup_cmd(nodes, renumbers_nodes, verbose)
         return self.execute_cmd(cmd, display_output, environment)
 
-    def clean(self, nodes: Optional[List[int]] = None, renumbers_nodes: bool = False, display_output: bool = False, environment: Optional[dict] = None):
-        return self.gcleanup(nodes, renumbers_nodes, display_output, environment)
+    def clean(self, nodes: Optional[List[int]] = None, renumbers_nodes: bool = False, display_output: bool = False,
+              verbose: bool = False, environment: Optional[dict] = None) -> int:
+        return self.gcleanup(nodes, renumbers_nodes, display_output, verbose, environment)
 
-    def get_gsub_cmd(self, nodes: Optional[List[int]] = None):
-        arguments = f'-n {self.get_nodes(nodes)}' if nodes else '-e all'
-        return f'gsub {arguments} {self.path}'
+    def get_gsub_cmd(self, nodes: Optional[List[int]] = None, verbose: bool = False) -> str:
+        scope = f'-n {self.get_nodes(nodes)}' if nodes else '-e all'
+        verbose = ' -verbose' if verbose else ''
+        command = ' '.join(['gsub', scope, verbose, self.path])
+        return command
 
-    def get_gcleanup_cmd(self, nodes: Optional[List[int]] = None, renumbers_nodes: bool = False):
+    def get_gcleanup_cmd(self, nodes: Optional[List[int]] = None, renumbers_nodes: bool = False,
+                         verbose: bool = False) -> str:
+        verbose = ' -verbose' if verbose else ''
         if renumbers_nodes:
-            return f'gcleanup -ren {self.path}'
-        arguments = f'-n {self.get_nodes(nodes)} ' if nodes else ''
-        return f'gsub {arguments} {self.path}'
+            return f'gcleanup -ren {self.path} {verbose}'
+        scope = f'-n {self.get_nodes(nodes)} ' if nodes else ''
+        command = ' '.join(['gcleanup', scope, verbose, self.path])
+        return command
 
-    def execute_cmd(self, cmd: str, display_output: bool = False, custom_environment: Optional[dict] = None):
+    def execute_cmd(self, cmd: str, display_output: bool = False, custom_environment: Optional[dict] = None) -> int:
         custom_environment = custom_environment or self.custom_environment
         stdout = None if display_output else subprocess.DEVNULL
         return subprocess.call([cmd], shell=True, stdout=stdout, env=custom_environment)
@@ -41,7 +51,7 @@ class Project:
     def get_nodes(nodes: List[int]) -> str:
         return ' '.join(map(str, nodes))
 
-    def set_environment(self, tcad_path, scl_path, license_path, stdb_path):
+    def set_environment(self, tcad_path: str, scl_path: str, license_path: str, stdb_path: str) -> None:
         env = os.environ.copy()
         env["PATH"] = tcad_path + ":" + env["PATH"]
         env["PATH"] = scl_path + ":" + env["PATH"]
